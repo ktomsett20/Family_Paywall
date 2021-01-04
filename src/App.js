b132ea4a60f2f4315d4464769122d7119beafe36
Adam Hurst <adam@logmycare.co.uk>
2021-01-04 14:33:50 +0000

    adding examples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderSection from './components/HeaderSection'
+
+
+function AboutFamilySection(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderSection />
+      <AboutFamilySection
+        title="Section 1"
+      />
+      <AboutFamilySection
+        title="Section 2"
+      />
     </div>
   );
 }
